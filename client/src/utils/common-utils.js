642accd4b2199f38b861570ce59ba2847ed4a827
yatin-kalia <yatinkalia1@gmail.com>
2023-12-14 17:29:08 +0530

    params update

--- client/src/utils/common-utils.js
+++ client/src/utils/common-utils.js
@@ -8,14 +8,14 @@ export const addElipsis=(str,limit)=>{
     return str.length>limit?str.substring(0,limit)+'...':str;  
 }
 export const getType=(value,body)=>{
-    if(value.params){
-        return{params:body}
-    }else if(value.query){
+    if(value.query){
+        return{query:body}
+    }else if(value.params){
         if(typeof body==='object'){
-            return {query:body._id}
+            return {params:body._id}
         }
         else{
-            return {query:body}
+            return {params:body}
         }
     }
     return {};
